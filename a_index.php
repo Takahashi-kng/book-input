4d56e876ac595418db7ab3cccf675a548c17c65b
Takahashi Kengo <kng25@ezweb.ne.jp>
2025-08-19 01:31:37 +0900

    Update a_index.php
    「関数ファイル削除に伴う変更」と「（未作成時の）テーブル作成の文を追加」

--- a_index.php
+++ a_index.php
@@ -1,6 +1,8 @@
 <?php
-  require 'common.php';
-  $s = connect();
+
+  $s = new PDO("mysql:host=localhost;dbname=db1","root","root");
+  $s->query("CREATE TABLE IF NOT EXISTS CRAFT01( NUMBER INT(3) AUTO_INCREMENT PRIMARY KEY,title VARCHAR(10),evalu INT(2),comment VARCHAR(10) DEFAULT '未入力')");
+
   $re=$s->query("SELECT title,comment FROM CRAFT01");
   $rireki = $re->fetchAll;
 
